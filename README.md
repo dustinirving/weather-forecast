81b026b38d1f73106fb6cdfa52707747192add0c
Dustin Irving <dustin.irving@gmail.com>
2020-03-18 11:48:24 -0400

    Added comments and updated the README file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+https://dustinirving.github.io/weather-forecast/
+
 # weather-forecast
+The main objective of this project was to make use of a server-side API to retrieve weather data. The user can choose a specified city and the DOM will be updated with current information regarding the weather for today as well as the next 5 days. The temperature,humidity, wind speed and UV index is displayed for the current date as well an icon relating to the weather conditions. For the 5-day forecast, a weather icon, the temperature and humidity are displayed.
+
+## Diving Deeper
+When the user opens the page weather data is first initialized with ottawa. The user can enter a city and click the search button. When the user clicks the button a button with the specified city is added to a list of buttons. The DOM is updated with that city's weather data. The user can then click on the appended buttons to change the weather information to that city.
+
+When the page is refreshed, the buttons are retrieved from local storage and appended to list of cities. Also, the weather information for the most recent city is displayed on the screen.
+
+For the current weather data, the UV index is color coded based on its value. If the UV index is greater or equal to 8, then it is red. If it is greater than 2, but less than 8, it is yellow. Finally, if it is less than 2, then it is green.
+
+## Visual Display
+
+![Weather Forecast](readmeimages/weather-forecast.png)
+
+## Future Endevers
+- Allow the user to compare weather properties between cities
+- Add a map with temperatures at different cities
+- Add a graph to display weather properties over a period of time
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Use Built in javascript functions to retrieve the date
+
 let d = new Date()
 let month = d.getMonth()
 let year = d.getFullYear()
@@ -6,20 +8,21 @@ let fullDate = '(' + month + '/' + dayOfTheMonth + '/' + year + ')'
 
 // Using API
 
+// Set the Api key to a variable 
 let APIkey = '8121ebc866b52ea8600081b69ea2ab51'
 
+// Retrieve the city name store in local storage or initialize it with ottawa
 let cityName = JSON.parse(localStorage.getItem("city")) || 'ottawa'
 
-
+// Define the query URL based on the city name
 let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + APIkey
 
 // Adding new cities
 
-let city = document.getElementById("cityInput")
 
-let search = document.getElementById("searchButton")
-
-
+// Retrieve the cities stored in local storage or make an empty list
+// Append buttons based on the list of cities in local storage
+// Add styling and the value attribute
 let cityButtonsArray = JSON.parse(localStorage.getItem("cities")) || []
 for (let i = 0; i < cityButtonsArray.length; i++) {
     let newCity = document.createElement("button")
@@ -30,6 +33,16 @@ for (let i = 0; i < cityButtonsArray.length; i++) {
     buttonsDiv.appendChild(newCity)
 }
 
+// Selector for the input element
+let city = document.getElementById("cityInput")
+
+// Selector for the search button
+let search = document.getElementById("searchButton")
+
+
+// Add an event listener for when the user clicks the search button
+// A new button is appended to the list and the API is called to set the DOM to weather information
+// The city added is also added to list in local storage
 search.addEventListener("click", function() {
     let newCity = document.createElement("button")
     let buttonsDiv = document.getElementById("buttons")
@@ -51,6 +64,8 @@ search.addEventListener("click", function() {
 let buttons = document.getElementById('buttons')
 let citySelect = document.querySelectorAll('.city')
 
+
+// Add an event listener to the city buttons to allow the user to update the page with the specified city with weather data
 buttons.addEventListener('click', function() {
     if (event.target.matches('.city')) {
         cityName = event.target.value
@@ -61,7 +76,7 @@ buttons.addEventListener('click', function() {
 })
 
 
-
+// Function to fetch the URL with weather data
 function callAPI() {
     fetch(queryURL)
         .then(function(response) {
@@ -119,49 +134,66 @@ function callAPI() {
                     let uvLable = document.getElementById("uv-label")
                     uvLable.textContent = 'UVIndex:'
                     uvIndexSelector.textContent = uvIndex
+                        // If the uv index is greater or equal to 8 the disabled button is red
                     if (uvIndex >= 8) {
                         uvIndexSelector.classList.add("bg-danger")
                         uvIndexSelector.classList.remove("bg-warning")
                         uvIndexSelector.classList.remove("bg-success")
-                    } else if (uvIndex < 8 && uvIndex > 2) {
+                    }
+                    // If the uv index is greater or equal to 8 the disabled button is yellow
+                    else if (uvIndex < 8 && uvIndex > 2) {
                         uvIndexSelector.classList.add("bg-warning")
                         uvIndexSelector.classList.remove("bg-success")
                         uvIndexSelector.classList.remove("bg-danger")
-                    } else {
+                    }
+                    // If the uv index is greater or equal to 8 the disabled button is green
+                    else {
                         uvIndexSelector.classList.add("bg-success")
                         uvIndexSelector.classList.remove("bg-danger")
                         uvIndexSelector.classList.remove("bg-warning")
                     }
                 })
-                // Updating 5-Day Forecast
+
+            // Updating 5-Day Forecast
             let day = 1
 
+            // While loop used to cycle through the 5-day forecast
             while (day < 6) {
+                // Date index used to retrieve the appropriate weather data for the next day
                 dateIndex += 7
+                    // Day is updated
                 dayOfTheMonth += 1
+                    // fullDate is constructed
                 fullDate = '(' + month + '/' + dayOfTheMonth + '/' + year + ')'
 
                 // weather icon
                 weatherIcon = weatherData.list[dateIndex].weather[0].icon
 
-                // Temp in kelvins
+                // Temp in kelvins converted to Farenheit
                 temperature = (Number((weatherData.list[dateIndex].main.temp) * 9 / 5 - 459.67)).toFixed(1)
 
                 // Humidity
                 humidity = weatherData.list[dateIndex].main.humidity
+
+                // Select the elements in the HTML
                 let dateSelector = document.getElementById("date" + day)
                 let weatherIconSelector = document.getElementById('weather-icon' + day)
                 let tempSelector = document.getElementById("temp" + day)
                 let humidSelector = document.getElementById("humid" + day)
+
+                // Update the date, weather icon, temperature and humidity on the page
                 dateSelector.textContent = fullDate
                 weatherIconSelector.setAttribute('src', 'http://openweathermap.org/img/wn/' + weatherIcon + '.png')
                 tempSelector.textContent = "Temp: " + temperature + '\u00B0 F'
                 humidSelector.textContent = "Humidity: " + humidity + '%'
+                    // increment the day and repeat for all 5 days
                 day += 1
 
             }
+            // Set the day back to the current date 
             dayOfTheMonth = d.getDate()
         })
 }
 
+// Call the API to initialize with the most recent
 callAPI()
